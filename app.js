29caa12395d32e484b5b5764c3adcede996e1697
HssanMahdi <mahdi.hssan@esprit.tn>
2022-03-02 23:12:55 +0100

    DB connection

--- app.js
+++ app.js
@@ -3,24 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("Connected to DB !!! && Server running on PORT : "+process.env.PORT))
+.catch(err=>console.log(err.message));
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
